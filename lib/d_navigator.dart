1a8858b33ad4a7e987877b19112980d171bfa9a3
dengsgo <dengsgo@gmail.com>
2019-03-25 18:20:35 +0800

    增加 注释

--- lib/d_navigator.dart
+++ lib/d_navigator.dart
@@ -2,12 +2,15 @@ library d_navigator;
 
 import 'package:flutter/widgets.dart';
 
+/// push type enums
 enum DNavigatorPushType {
   push,
   popAndPush,
   pushReplace,
 }
 
+/// query base object
+/// If you want to customize the field, inherit this class and extend it yourself.
 class DNavigatorQuery {
   DNavigatorQuery({
     this.id,
@@ -16,14 +19,42 @@ class DNavigatorQuery {
   });
 
   final Object id;
+
+  /// Built-in verification authorization field.
+  /// The default is false.
+  /// If set to true, you need to call setAuthorizedHandlerFunc in DNavigator
+  /// to set the callback method.
   final bool mustAuthorize;
+
+  /// Built-in hero animation tag.
+  /// you can receive this parameter on the page you want to jump,
+  /// passed to the Hero tag widget
   final String heroTag;
 }
 
+/// typedef params
 typedef PageRoute<T> DQueryPageRoute<T extends Object>(DNavigatorQuery query);
 typedef Future<bool> BoolCallback();
 typedef Future<Object> ObjectCallback();
 
+/// DNavigator class
+/// To use this component, you need to register the page into the component first.
+/// ```dart
+/// DNavigator.registerNameRoute("/page-a", (DNavigatorQuery query) {
+//    return new MaterialPageRoute(
+//        builder: (BuildContext context) {
+//          return PageB(query);
+//        }
+//    );
+//  });
+/// ```
+/// Then you can use the goNamed method to jump to the page.
+/// ```dart
+/// DNavigator.of(context).goNamed("/page-a", DNavigatorQuery(
+//      id: 10086,
+//  ));
+/// ```
+///
 class DNavigator {
   DNavigator(this.navigatorState) :
         assert(navigatorState != null),
@@ -35,12 +66,21 @@ class DNavigator {
   BoolCallback _isAuthorizedHandler;
   ObjectCallback _goAuth;
 
+  /// Routing map
   static Map<String, DQueryPageRoute> namedRoutesMapping = <String, DQueryPageRoute>{};
 
+  /// Initialization component
+  /// ```dart
+  /// DNavigator.of(context);
+  /// ```
   static DNavigator of(BuildContext context) {
     return DNavigator(Navigator.of(context));
   }
 
+  /// Initialization component
+  /// ```dart
+  /// DNavigator.from(Navigator.of(context));
+  /// ```
   static DNavigator from(NavigatorState state) {
     return DNavigator(state);
   }
